85a113128705eae7c9e944c1bd1ed8a538e72915
River Wang <urfdvw@gmail.com>
2024-06-01 17:44:19 -0400

    combined features

--- code.py
+++ code.py
@@ -1,1 +1,33 @@
-from test_gcode import *
+import time
+import board
+from connected_variables import ConnectedVariables
+
+from drawing_robot import DrawingRobot, drawGcode
+
+robot = DrawingRobot(
+    board.GP4,
+    board.GP5,
+    board.GP6,
+    23.2,
+    30.0,
+    60.0,
+    12.5
+)
+
+#%%
+robot.move(False, 0, 0)
+
+cv = ConnectedVariables()
+cv.define('draw_pos', {'x': 0, 'y': 50, 'z': 0})
+cv.define('file', '')
+
+while True:
+    if cv.read('file'):
+        # try:
+        drawGcode(robot, cv.read('file') + '.gcode')
+        # except:
+        #     pass
+        cv.write('file', '')
+    else:
+        pos = cv.read('draw_pos')
+        robot.move_xy(bool(pos['z']), pos['x'], pos['y'])
--- readme.md
+++ readme.md
@@ -6,3 +6,28 @@
 # Video
 
 [![](https://img.youtube.com/vi/k056yo-cRJI/0.jpg)](https://www.youtube.com/watch?v=k056yo-cRJI)
+
+# Docs
+
+robot arm motor specs
+- left
+    - pin: GP5
+    - 0: reach
+    - 90: back
+    - drift: 0
+- right
+    - pin: GP6
+    - 0: back
+    - 90: reach
+    - drift: + 80
+- lift
+    - pin: GP4
+    - 60: pen down
+    - 0: pen raise
+    - drift: 0
+
+robot arm dimensions
+- a: 23.2
+- b: 30.0
+- c: 60.0
+- d: 12.5
